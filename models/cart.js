81abbbb827cd71d3697dbb9131146c4c1732a220
Mehrsa <mhrs.project22@gmail.com>
2024-01-07 13:07:31 +0330

    create cart component

--- models/cart.js
+++ models/cart.js
@@ -12,12 +12,58 @@ class Cart {
         this.parent.innerHTML = "";
         this.toShow.forEach((product) => {
             const qty = this.products.filter((p) => p.id === product.id).length;
-            this.createCard(product, qty);
+            this.createCart(product, qty);
         });
     }
 
-    createCard(data, qty) {
-        console.log(data);
+    createCart(data, qty) {
+        const cartEle = document.createElement("div");
+
+        const imgEle = this.productImg(data);
+        const infoEle = this.productInfo(data);
+        const controlEle = this.productControl(data, qty);
+
+        cartEle.innerHTML = imgEle;
+        cartEle.innerHTML += infoEle;
+        cartEle.innerHTML += controlEle;
+
+        this.parent.appendChild(cartEle);
+    }
+
+    productImg(data) {
+        const { image, alt } = data;
+
+        const imgJSX = `<img alt=${alt} src=${image} />`;
+
+        return imgJSX;
+    }
+    productInfo(data) {
+        const { name, price } = data;
+
+        const infoJSX = `
+            <div id="cart-info">
+                <h4>${name}</h4>
+                <p>$ ${price}</p>
+            </div>
+        `;
+
+        return infoJSX;
+    }
+    productControl(data, qty) {
+        const { id } = data;
+
+        const controlJSX = `
+            <div id="cart-control">
+                <div>
+                    <button data-id=${id}>-</button>
+                    <span>${qty}</span>
+                    <button data-id=${id}>+</button>
+                </div>
+                <button data-id=${id}>Remove</button>
+            </div>
+        `;
+
+        return controlJSX;
     }
 }
 
